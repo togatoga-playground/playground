a6016176cb59de3be62878148448ac4c7bd47a28
togatoga <togasakitogatoga@gmail.com>
2022-07-04 21:24:26 +0900

    Update

---
+++ distributed-services-with-go/internal/log/store_test.go
@@ -0,0 +1,36 @@
+package log
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	write = []byte("hello world")
+	width = uint64(len(write)) + lenWidth
+)
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_append_read_test")
+	require.NoError(t, err)
+	s, err := newStore(f)
+	require.NoError(t, err)
+	testAppend(t, s)
+	//testRead(t, s)
+	//testReadAt(t, s)
+
+	s, err = newStore(f)
+	require.NoError(t, err)
+	//testRead(t, s)
+}
+
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+		require.Equal(t, pos+n, width*i)
+	}
+}
