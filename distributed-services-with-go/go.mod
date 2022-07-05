fddb057c3b42fcc7a04c6d6e85247fa8a072bff2
togatoga <togasakitogatoga@gmail.com>
2022-07-05 22:20:25 +0900

    Update

--- distributed-services-with-go/go.mod
+++ distributed-services-with-go/go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/tysonmote/gommap v0.0.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ distributed-services-with-go/internal/log/index.go
@@ -0,0 +1,50 @@
+package log
+
+import (
+	"os"
+
+	"github.com/tysonmote/gommap"
+)
+
+var (
+	offWidth uint64 = 4
+	posWidth uint64 = 8
+	entWidth        = offWidth + posWidth
+)
+
+type index struct {
+	file *os.File
+	mmap gommap.MMap
+	size uint64
+}
+
+func newIndex(f *os.File, c Config) (*index, error) {
+	idx := &index{
+		file: f,
+	}
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	idx.size += uint64(fi.Size())
+	if err = os.Truncate(f.Name(), int64(c.Segment.MaxIndexBytes)); err != nil {
+		return nil, err
+	}
+	if idx.mmap, err = gommap.Map(idx.file.Fd(), gommap.PROT_READ|gommap.PROT_WRITE, gommap.MAP_SHARED); err != nil {
+		return nil, err
+	}
+	return idx, nil
+}
+
+func (i *index) Close() error {
+	if err := i.mmap.Sync(gommap.MS_SYNC); err != nil {
+		return err
+	}
+	if err := i.file.Sync(); err != nil {
+		return err
+	}
+	if err := i.file.Truncate(int64(i.size)); err != nil {
+		return err
+	}
+	return i.file.Close()
+}
