52112178acb2492e5a638a70eec86422737f2908
togatoga <togasakitogatoga@gmail.com>
2022-07-10 23:01:20 +0900

    Update

--- concurrency-in-go/chap4/fan-out-fan-in/main.go
+++ concurrency-in-go/chap4/fan-out-fan-in/main.go
@@ -1,8 +1,32 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+	"time"
+)
 
 func main() {
+
+	take := func(done <-chan interface{}, valueStream <-chan interface{}, num int) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := num; i > 0 || i == -1; {
+				if i != -1 {
+					i--
+				}
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
 
 	fanIn := func(done <-chan interface{}, channels ...<-chan interface{}) <-chan interface{} {
 		var wg sync.WaitGroup
@@ -28,5 +52,78 @@ func main() {
 		}()
 		return multiplexedStream
 	}
+	repeatFn := func(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				select {
+				case <-done:
+					return
+				case valueStream <- fn():
+				}
+			}
+		}()
+		return valueStream
+	}
+	toInt := func(done <-chan interface{}, valueStream <-chan interface{}) <-chan int {
+		intStream := make(chan int)
+		go func() {
+			defer close(intStream)
+			for v := range valueStream {
+				select {
+				case <-done:
+					return
+				case intStream <- v.(int):
+				}
+			}
+		}()
+		return intStream
+	}
+	primeFinder := func(done <-chan interface{}, intStream <-chan int) <-chan interface{} {
+		primeStream := make(chan interface{})
+		go func() {
+			defer close(primeStream)
+
+			for {
+
+				select {
+				case <-done:
+					return
+				case x := <-intStream:
+					isPrime := true
+					for i := 2; i < x; i++ {
+
+						if x%i == 0 {
+							isPrime = false
+							break
+						}
+					}
+					if isPrime && x != 1 {
+						primeStream <- x
+					}
+				}
+			}
+		}()
+		return primeStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	start := time.Now()
+	rand := func() interface{} { return rand.Intn(50000000) }
+	randInstream := toInt(done, repeatFn(done, rand))
+	numFinders := runtime.NumCPU()
+	fmt.Printf("Spinning up %d prime finders.\n", numFinders)
+	finders := make([]<-chan interface{}, numFinders)
+	fmt.Println("Primes:")
+	for i := 0; i < numFinders; i++ {
+		finders[i] = primeFinder(done, randInstream)
+	}
+	for prime := range take(done, fanIn(done, finders...), 20) {
+		fmt.Println(prime)
+	}
+	fmt.Printf("Search took: %v", time.Since(start))
 
 }
