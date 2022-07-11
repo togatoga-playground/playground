4e09d96f704b49bcf7fb7120fc517148a180f265
togatoga <togasakitogatoga@gmail.com>
2022-07-11 15:12:28 +0900

    Update

---
+++ concurrency-in-go/chap4/go.mod
@@ -0,0 +1,3 @@
+module concurrency-in-go/chap4
+
+go 1.18
---
+++ concurrency-in-go/chap4/lib/lib.go
@@ -0,0 +1,23 @@
+package lib
+
+func OrDone(done, c <-chan interface{}) <-chan interface{} {
+	valStream := make(chan interface{})
+	go func() {
+		defer close(valStream)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-c:
+				if !ok {
+					return
+				}
+				select {
+				case <-done:
+				case valStream <- v:
+				}
+			}
+		}
+	}()
+	return valStream
+}
