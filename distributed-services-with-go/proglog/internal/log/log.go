ed66b5829d0c18f9f218e4a3361798d2e1b45eee
togatoga <togasakitogatoga@gmail.com>
2022-07-26 22:17:02 +0900

    Add log_test.go

--- distributed-services-with-go/proglog/internal/log/log.go
+++ distributed-services-with-go/proglog/internal/log/log.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path"
@@ -150,6 +151,27 @@ func (l *Log) Truncate(lowest uint64) error {
 	}
 	l.segments = segments
 	return nil
+}
+
+func (l *Log) Reader() io.Reader {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	readers := make([]io.Reader, len(l.segments))
+	for i, segment := range l.segments {
+		readers[i] = &originReader{segment.store, 0}
+	}
+	return io.MultiReader(readers...)
+}
+
+type originReader struct {
+	*store
+	off int64
+}
+
+func (o *originReader) Read(p []byte) (int, error) {
+	n, err := o.ReadAt(p, o.off)
+	o.off += int64(n)
+	return n, err
 }
 
 func (l *Log) newSegment(off uint64) error {
