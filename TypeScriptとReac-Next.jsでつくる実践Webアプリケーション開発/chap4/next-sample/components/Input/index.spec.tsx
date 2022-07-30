2673275f5210a0a1d66e8f08a0eff02965348aae
togatoga <togasakitogatoga@gmail.com>
2022-07-30 19:03:47 +0900

    Update

--- TypeScriptとReac-Next.jsでつくる実践Webアプリケーション開発/chap4/next-sample/components/Input/index.spec.tsx
+++ TypeScriptとReac-Next.jsでつくる実践Webアプリケーション開発/chap4/next-sample/components/Input/index.spec.tsx
@@ -1,4 +1,4 @@
-import {render, screen, RenderResult} from '@testing-library/react'
+import {render, screen, RenderResult, fireEvent} from '@testing-library/react'
 import { Input } from '.'
 describe('Input', () => {
     let renderResult: RenderResult
@@ -13,5 +13,18 @@ describe('Input', () => {
     it('should empty in input on initial render', () => {
         const inputNode = screen.getByLabelText('Username') as HTMLInputElement
         expect(inputNode).toHaveValue('')
+    })
+
+    it('should show input text', () => {
+        const inputText = 'Test Input Text'
+        const inputNode = screen.getByLabelText('Username') as HTMLInputElement
+        fireEvent.change(inputNode, {target: {value: inputText}})
+
+        expect(inputNode).toHaveValue(inputText)
+    })
+
+    it('should reset when user clicks button', async() => {
+        const inputTeext = 'Test Input Text'
+        const inputNode = screen.getByLabelText('Username') as HTMLInputElement
     })
 })
