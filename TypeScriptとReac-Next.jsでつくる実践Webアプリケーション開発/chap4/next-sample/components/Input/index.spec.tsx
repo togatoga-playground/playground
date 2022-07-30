bc977819e1e1dabc0ee2d9a736df5817ccae5a9a
togatoga <togasakitogatoga@gmail.com>
2022-07-30 18:33:58 +0900

    Update

---
+++ TypeScriptとReac-Next.jsでつくる実践Webアプリケーション開発/chap4/next-sample/components/Input/index.spec.tsx
@@ -0,0 +1,17 @@
+import {render, screen, RenderResult} from '@testing-library/react'
+import { Input } from '.'
+describe('Input', () => {
+    let renderResult: RenderResult
+    beforeEach(() => {
+        renderResult = render(<Input id="username" label="Username"/>)
+    }) 
+
+    afterEach(() => {
+        renderResult.unmount()
+        
+    }) 
+    it('should empty in input on initial render', () => {
+        const inputNode = screen.getByLabelText('Username') as HTMLInputElement
+        expect(inputNode).toHaveValue('')
+    })
+})
