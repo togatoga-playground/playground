3b09cb02badf022d395bb734a2cff9df34e85cb5
togatoga <togasakitogatoga@gmail.com>
2022-08-01 15:48:57 +0900

    Update

---
+++ TypeScriptとReac-Next.jsでつくる実践Webアプリケーション開発/chap5/nextjs-gihyo-book/src/themes/index.ts
@@ -0,0 +1,12 @@
+import colors from './colors'
+import fontSizes from './fontSizes'
+import letterSpacings from './letterSpacings'
+import lineHeights from './lineHeights'
+import space from './space'
+export const theme = {
+  space,
+  fontSizes,
+  letterSpacings,
+  lineHeights,
+  colors,
+} as const
