1bf8acc81cd81a13658b75cede0f44cf9a7ea012
togatoga <togasakitogatoga@gmail.com>
2022-08-01 16:13:22 +0900

    Update

--- TypeScriptとReac-Next.jsでつくる実践Webアプリケーション開発/chap5/nextjs-gihyo-book/.storybook/main.js
+++ TypeScriptとReac-Next.jsでつくる実践Webアプリケーション開発/chap5/nextjs-gihyo-book/.storybook/main.js
@@ -10,4 +10,20 @@ module.exports = {
     builder: "@storybook/builder-webpack5",
   },
   staticDirs: ["public"],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-private-methods",
+      "@babel/plugin-proposal-private-property-in-object",
+    ],
+  }),
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      }),
+    ];
+    return config;
+  },
 };
