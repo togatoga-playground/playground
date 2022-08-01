b870983adfe340debe479d54137642a0c2af9c40
togatoga <togasakitogatoga@gmail.com>
2022-08-01 09:38:21 +0900

    npm run format

--- TypeScriptとReac-Next.jsでつくる実践Webアプリケーション開発/chap5/nextjs-gihyo-book/src/pages/_app.tsx
+++ TypeScriptとReac-Next.jsでつくる実践Webアプリケーション開発/chap5/nextjs-gihyo-book/src/pages/_app.tsx
@@ -1,8 +1,7 @@
-import "../styles/globals.css";
-import { AppProps } from "next/app";
-import { createGlobalStyle } from "styled-components";
-import Head from "next/head";
-
+import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
 html,
@@ -28,21 +27,25 @@ color: #000;
 ol, ul {
 list-style: none;
 }
-`;
+`
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta key="charset" name="charset" content="utf-8" />
-        <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"/>
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+        />
         <meta property="og:locale" content="ja_JP" />
         <meta property="og:type" content="website" />
       </Head>
-      {/* <GlobalStyle /> */}
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
