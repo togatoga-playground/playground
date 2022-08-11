595b870d79eaa50b3d7437a005019fac8a713dbe
togatoga <togasakitogatoga@gmail.com>
2022-08-11 16:54:09 +0900

    Update

---
+++ TypeScriptとReac-Next.jsでつくる実践Webアプリケーション開発/chap5/nextjs-gihyo-book/src/services/users/get-user.ts
@@ -0,0 +1,27 @@
+import { ApiContext, User } from '../../types/data'
+import { fetcher } from '../../utils'
+
+export type GetUserParams = {
+    id: number
+}
+
+/**
+ * User API
+ * @param context API context
+ * @param params paramter
+ * @returns user
+ */
+const getUser = async (
+    context: ApiContext,
+    { id }: GetUserParams,
+): Promise<User> => {
+    return await fetcher(
+        `${context.apiRootUrl.replace(/\/$/g, '')}/users/${id}}`,
+        {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        },
+    )
+}
