0ec56a16a963a8d7df346db0cb7367205d1d802f
togatoga <togasakitogatoga@gmail.com>
2022-10-18 19:31:13 +0900

    WIP

--- zero2prod/src/routes/health_check.rs
+++ zero2prod/src/routes/health_check.rs
@@ -1,17 +1,5 @@
-use std::net::TcpListener;
+use actix_web::{HttpResponse, Responder};
 
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
--- zero2prod/src/startup.rs
+++ zero2prod/src/startup.rs
@@ -1,3 +1,8 @@
+use actix_web::{dev::Server, web, App, HttpServer};
+use std::net::TcpListener;
+
+use crate::routes::{health_check, subscribe};
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
