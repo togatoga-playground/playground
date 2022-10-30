73e215157a2fc374f83a2a429160dc7b0677a4a5
togatoga <togasakitogatoga@gmail.com>
2022-10-30 22:30:09 +0900

    Update

--- zero2prod/src/configuration.rs
+++ zero2prod/src/configuration.rs
@@ -1,4 +1,5 @@
 use secrecy::{ExposeSecret, Secret};
+use serde_aux::field_attributes::deserialize_number_from_string;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -8,6 +9,7 @@ pub struct Settings {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -16,6 +18,7 @@ pub struct ApplicationSettings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
@@ -57,7 +60,6 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .unwrap_or("local".into())
         .try_into()
         .expect("Failed to parse APP_ENVIRONMENT");
-
     let environment_filename = format!("{}.yaml", environment.as_str());
 
     let settings = config::Config::builder()
@@ -67,6 +69,11 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .add_source(config::File::from(
             configuration_directory.join(&environment_filename),
         ))
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
         .build()?;
     settings.try_deserialize::<Settings>()
 }
