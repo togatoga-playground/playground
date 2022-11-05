19bd3b3d9afd90d845397959f146111f0846800a
togatoga <togasakitogatoga@gmail.com>
2022-11-05 17:03:40 +0900

    Update

--- zero2prod/src/configuration.rs
+++ zero2prod/src/configuration.rs
@@ -5,10 +5,25 @@ use sqlx::{
     ConnectOptions,
 };
 
+use crate::domain::SubscriberEmail;
+
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
+    pub email_client: EmailClientSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct EmailClientSettings {
+    pub base_url: String,
+    pub sender_email: String,
+}
+
+impl EmailClientSettings {
+    pub fn sender(&self) -> Result<SubscriberEmail, String> {
+        SubscriberEmail::parse(self.sender_email.clone())
+    }
 }
 
 #[derive(serde::Deserialize)]
--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -3,7 +3,7 @@ use std::net::TcpListener;
 use zero2prod::{
     configuration::get_configuration,
     startup::run,
-    telemetry::{get_subscriber, init_subscriber},
+    telemetry::{get_subscriber, init_subscriber}, email_client::EmailClient,
 };
 
 #[tokio::main]
@@ -14,11 +14,13 @@ async fn main() -> std::io::Result<()> {
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
+    let sender_email = configuration.email_client.sender().expect("Invalid sender email address.");
+    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
 
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address).expect("Failed to bind an address");
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
